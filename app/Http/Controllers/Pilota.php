d1134bd6805f98cd01dc0b09b8f78ded026ae408
Kottra Richárd <kottra.richard@jedlik.eu>
2022-01-21 13:21:56 +0100

    sql kibővítése

--- app/Http/Controllers/Pilota.php
+++ app/Http/Controllers/Pilota.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\DB;
 class Pilota extends Controller
 {
     public function getPilotak() {
-        return DB::select("SELECT * FROM versenyzok");
+        return DB::select("SELECT versenyzok.ID, versenyzok.nev, versenyzok.nemzet,
+                            versenyzok.szuletes, versenyzok.magassag, csapatok.csapatid,
+                            csapatok.csapatnev, csapatok.nemzet AS 'csapatnemzet'
+                            FROM versenyzok
+                            INNER JOIN csapatok ON csapatok.csapatid = versenyzok.csapat");
     }
 }
